bc3bac8d64dfcaa72c1616626d5d0481e139bc81
SonuThorave <96857813+SonuThorave@users.noreply.github.com>
2022-03-02 21:59:54 -0800

    updateb21

---
+++ operator.py
@@ -0,0 +1,13 @@
+a=10
+b=50
+c=0
+c=a+b
+print("addition of num :",c)
+c=a-b
+print("subtraction of num :",c)
+c=a*b
+print("multiplication of num :",c)
+c=a/b
+print("division of num :",c)
+c=a%b
+print("modulus of num :",c)
